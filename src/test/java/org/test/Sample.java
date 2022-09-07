325bdb4e3ad3df3420695d46b15b692a2457f806
manikandan20121998 <manimaddy978@gmail.com>
2022-09-07 00:30:49 -0700

    mani's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -3,7 +3,9 @@ package org.test;
 public class Sample {
 	
 	public static void main(String[] args) {
-		
-	}
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		}
 
 }
